2070f2fcfbed9034c4ca4eec64b9960b29cd2111
ljingGBC <lei.jing@georgebrown.ca>
2021-04-06 13:33:50 -0400

    add PlayGame and Quit functions for the ManiMenu

---
+++ Chaos-Adventure_AllWorlds/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//import scene management
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game Successfully!");// show info in console
+        Application.Quit();
+    }
+}
