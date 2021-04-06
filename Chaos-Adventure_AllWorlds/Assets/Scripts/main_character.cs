053df22397a29615aa3ff65020f6c0c6edb1b8e5
ljingGBC <lei.jing@georgebrown.ca>
2021-04-06 13:44:22 -0400

    test and debug, update attack and movement functions

--- Chaos-Adventure_AllWorlds/Assets/Scripts/main_character.cs
+++ Chaos-Adventure_AllWorlds/Assets/Scripts/main_character.cs
@@ -4,142 +4,114 @@ using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 
-public class main_character : MonoBehaviour {
+public class main_character : MonoBehaviour
+{
 
-	// variables for character
-	float dirX, dirY;
-    //float rotateAngle; // rotate gun
+    // variables for character
+    float dirX, dirY; // rotate character
 
     [SerializeField]
-	float moveSpeed = 2f;
+    float moveSpeed = 2f;
 
 
-    /* GUN
-	[SerializeField]
-	float bulletSpeed = 5f;
-	*/
+    // BOW
+    float rotateAngle; // rotate gun
+
+    [SerializeField]
+    float arrowSpeed = 5f;
 
     Animator anim;
 
-	/*
-	// Animator variable to control transitions
-	[SerializeField]
-	Transform gun;
-	[SerializeField]
-	Rigidbody2D bullet;
-	*/
+
+    // Animator variable to control transitions
+    [SerializeField]
+    Transform bow;
+    [SerializeField]
+    Rigidbody2D arrow;
 
 
-	// Start is called before the first frame update
+
+    // Start is called before the first frame update
     void Start()
     {
-        //rotateAngle = 0f;
-        anim = GetComponent<Animator> ();
+        rotateAngle = 0f;
+        anim = GetComponent<Animator>();
         anim.speed = 1;
 
     }
 
-	
+
     // Update is called once per frame
     void Update()
     {
-        Move ();
-        //Fire ();
-        Rotate ();
+        Move();
+        Fire();
+        Rotate();
 
     }
 
     void Move()
     {
-    	dirX = Mathf.RoundToInt(CrossPlatformInputManager.GetAxis ("Horizontal"));
-    	dirY = Mathf.RoundToInt(CrossPlatformInputManager.GetAxis ("Vertical"));
+        dirX = Mathf.RoundToInt(CrossPlatformInputManager.GetAxis("Horizontal"));
+        dirY = Mathf.RoundToInt(CrossPlatformInputManager.GetAxis("Vertical"));
 
-    	transform.position = new Vector2 (dirX * moveSpeed * Time.deltaTime + transform.position.x,
-    		dirY * moveSpeed * Time.deltaTime + transform.position.y);
+        transform.position = new Vector2(dirX * moveSpeed * Time.deltaTime + transform.position.x,
+            dirY * moveSpeed * Time.deltaTime + transform.position.y);
     }
 
-    /*
     void Fire()
     {
-    	if (CrossPlatformInputManager.GetButtonDown ("Fire1")) {
-    		var firedBullet = Instantiate (bullet, gun.position, gun.rotation);
-    		firedBullet.AddForce (gun.up * bulletSpeed);
-    	}
+        if (CrossPlatformInputManager.GetButtonDown("Attack"))
+        {
+            var firedArrow = Instantiate(arrow, bow.position, bow.rotation);
+            firedArrow.AddForce(bow.up * arrowSpeed);
+        }
     }
-    */
 
     // animation control and gun rotation happens here
     void Rotate()
     {
-    	// up
-    	if (dirX == 0 && dirY == 1) {
-    		//rotateAngle = 0;
-    		anim.speed = 1;
-    		anim.SetInteger ("Direction", 1);
-    	}
+        // up
+        if (dirX == 0 && dirY == 1)
+        {
+            rotateAngle = 0;
+            anim.speed = 1;
+            anim.SetInteger("Direction", 1);
+        }
 
-    	/* 8-dir up-right
-    	if (dirX == 1 && dirY == 1) {
-    		//rotateAngle = -45f;
-    		anim.speed = 1;
-    		anim.SetInteger ("Direction", 2);
-    	}
-    	*/
+        // right
+        if (dirX == 1 && dirY == 0)
+        {
+            rotateAngle = -90f;
+            anim.speed = 1;
+            anim.SetInteger("Direction", 2);
+        }
 
-    	// right
-    	if (dirX == 1 && dirY == 0) {
-    		//rotateAngle = -90f;
-    		anim.speed = 1;
-    		anim.SetInteger ("Direction", 2);
-    	}
+        // down
+        if (dirX == 0 && dirY == -1)
+        {
+            rotateAngle = -180f;
+            anim.speed = 1;
+            anim.SetInteger("Direction", 3);
+        }
 
-    	/* 8-dir down-right
-    	if (dirX == 1 && dirY == -1) {
-    		rotateAngle = -135f;
-    		anim.speed = 1;
-    		anim.SetInteger ("Direction", 4);
-    	}
-    	*/
+        // left
+        if (dirX == -1 && dirY == 0)
+        {
+            rotateAngle = -270f;
+            anim.speed = 1;
+            anim.SetInteger("Direction", 4);
+        }
 
-    	// down
-    	if (dirX == 0 && dirY == -1) {
-    		//rotateAngle = -180f;
-    		anim.speed = 1;
-    		anim.SetInteger ("Direction", 3);
-    	}
+        // idle
+        if (dirX == 0 && dirY == 0)
+        {
+            anim.speed = 0;
+            //anim.SetInteger("Direction", 0);
+        }
 
-    	/* 8-dir down-left
-    	if (dirX == -1 && dirY == -1) {
-    		//rotateAngle = -225f;
-    		anim.speed = 1;
-    		anim.SetInteger ("Direction", 6);
-    	}
-    	*/
-    	
-    	// left
-    	if (dirX == -1 && dirY == 0) {
-    		//rotateAngle = -270f;
-    		anim.speed = 1;
-    		anim.SetInteger ("Direction", 4);
-    	}
-
-    	/* 8-dir up-left
-    	if (dirX == -1 && dirY == 1) {
-    		//rotateAngle = -315f;
-    		anim.speed = 1;
-    		anim.SetInteger ("Direction", 8);
-    	}
-    	*/
-    	
-        
-    	// idle
-    	if (dirX == 0 && dirY == 0) {
-    		anim.speed = 0;
-    		anim.SetInteger ("Direction", 0);
-    	}
-
-    	// sets on an angle based according to character's direction
-    	// gun.rotation = Quaternion.Euler (0f, 0f, rotateAngle);
+        // sets on an angle based according to character's direction
+        bow.rotation = Quaternion.Euler(0f, 0f, rotateAngle);
     }
 
 }
