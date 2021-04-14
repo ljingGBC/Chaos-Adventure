291116c88f0e7eab04c2521542a2bd33254061df
jesmuli <jesronnecharisse.muli@georgebrown.ca>
2021-04-14 13:38:35 -0400

    create OptionsMenu,VolumeSlider and function, add BGM in MainMenuScene

---
+++ Chaos-Adventure_AllWorlds 2/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//import Audio
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetVolume (float volume)
+    {
+        Debug.Log(volume);//show change in console
+        audioMixer.SetFloat("mainVolume", volume);//"mainVolume" must match the name of Exposed Parameters in Audion Mixer
+    }
+}
