5c754a1dc3de57ed435bc989e182ef55bd684dff
ljingGBC <lei.jing@georgebrown.ca>
2021-04-14 16:55:51 -0400

    create NPC dialog and sign dialog, then implement the functions

---
+++ Chaos-Adventure_AllWorlds/Assets/Scripts/DialogFarmer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class DialogFarmer : MonoBehaviour
+{
+    public GameObject dialogBox;
+    public Text dialogText;
+    public string dialog;
+    public bool dialogActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dialogActive)
+        {
+            dialogText.text = dialog;
+            dialogBox.GetComponent<Image>().enabled = true;
+        }
+        else
+        {
+            //dialogBox.SetActive(false);
+            dialogText.text = "";//hide text
+            dialogBox.GetComponent<Image>().enabled = false;//hide dialog box picture
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player is in range");
+            dialogActive = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player left range");
+            dialogActive = false;
+            dialogBox.SetActive(false);
+        }
+    }
+}
